30ae14aa8c7f4c2a7fa08ce450776df51b23f7db
Devin Cooper <dcoope12@asu.edu>
2020-09-10 02:29:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,6 @@
 def reverse_each_word(sentence)
-sentence.each |word| do
-  word.reverse()
+  words_array = sentence.split(" ")
+  new_array = []
+  new_array = words_array.collect{|word| word.reverse}
+  new_array.join(" ")
 end
